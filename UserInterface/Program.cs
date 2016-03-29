b38241cf7c075267835beffb0e3db88e5f5713bd
TimVervers <timververs@msn.com>
2016-03-29 14:46:19 +0200

    Adding userinterface and renaming solution

---
+++ UserInterface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+
+namespace UserInterface
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(printName.name);
+			Console.ReadKey();
+		}
+	}
+}
